6f06893c845bb01202862234feec4e4e8771c17c
testdev <83214438+DanteDEVS@users.noreply.github.com>
2022-10-23 16:15:59 +0200

    Create mysql.sql

---
+++ resources/mysql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS dctx_stats (
+  name VARCHAR(255) PRIMARY KEY,
+  breaks INT DEFAULT 0,
+  places INT DEFAULT 0,
+  deaths INT DEFAULT 0,
+  kicked INT DEFAULT 0,
+  drops INT DEFAULT 0,
+  joins INT DEFAULT 0,
+  quits INT DEFAULT 0,
+  chats INT DEFAULT 0,
+  kills INT DEFAULT 0,
+  wins INT DEFAULT 0
+);
